8744890a534129b7c71d45ef4c74e3e185be81ff
Agustin Landriel <agusrojo20@hotmail.com>
2026-07-18 13:55:02 -0300

    Fix en use_lockfile en backend.tf y se cambio el provider de staging de 5.0 a 6.0

--- environments/staging/backend.tf
+++ environments/staging/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "voting-app-terraform-state-325503636955"
-    key            = "staging/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "voting-app-terraform-lock"
-    encrypt        = true
+    bucket       = "voting-app-terraform-state-325503636955"
+    key          = "staging/terraform.tfstate"
+    region       = "us-east-2"
+    use_lockfile = true
+    encrypt      = true
   }
 }
